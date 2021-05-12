feed7314f5a7d20d734aced0c16dd3ba456e2b32
[JAXMES] <[devers@hotmail.es]>
2021-05-12 18:50:38 -0400

    First attempt

---
+++ design-pad/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import EditButton from './components/editButton';
+
+function App() {
+  return (
+    <div className="App">
+        <EditButton />
+    </div>
+  );
+}
+
+export default App;
